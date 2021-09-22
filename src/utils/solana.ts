0abcbca574b2607dfe4a4facce90ab9787f6ac77
Zicklag <zicklag@katharostech.com>
2021-09-21 20:51:34 -0500

    Switch Solana URL to Devnet

--- src/utils/solana.ts
+++ src/utils/solana.ts
@@ -1,5 +1,6 @@
 import { Connection } from '@solana/web3.js';
 
-export const solanaUrl = 'http://localhost:8899';
+export const solanaUrl = 'https://api.devnet.solana.com';
+// export const solanaUrl = 'http://localhost:8899';
 
 export const connection = new Connection(solanaUrl, 'confirmed');
